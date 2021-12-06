eac2dd3646410b4ee1912c513ea0e25db70075fe
Dale Spencer <thefallout4fan@gmail.com>
2021-12-06 12:58:45 -0500

    Knife Changes
    * Added rotation to the knives
    * Changed location of where knife is thrown from
    * Added force to thrown knives

--- Webb Game/Assets/Scripts/Weapons/Throwing Knife/throwKnife.cs
+++ Webb Game/Assets/Scripts/Weapons/Throwing Knife/throwKnife.cs
@@ -6,6 +6,9 @@ public class throwKnife : MonoBehaviour
 {
     [SerializeField] public GameObject knife;
     [SerializeField] public GameObject player;
+    [SerializeField] public float force;
+    [SerializeField] public float torque;
+    [SerializeField] public int maxSpin;
     void Start()
     {
         
@@ -15,7 +18,11 @@ public class throwKnife : MonoBehaviour
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Mouse2)){
-            Instantiate(knife, transform.position+(transform.forward * 2), transform.rotation);
+            var newInstance = Instantiate(knife, transform.position+(transform.forward) + new Vector3(0,0.45f,0), (transform.rotation));
+            Rigidbody instance = newInstance.GetComponent<Rigidbody>();
+            instance.maxAngularVelocity = maxSpin;
+            instance.AddRelativeForce(Vector3.forward * force);
+            instance.AddRelativeTorque(torque, 0, 0);
         }
     }
 }
