ca7386dcbcc7c9e9f9a2cfcc975fa85dd4cdb2eb
Dale Spencer <thefallout4fan@gmail.com>
2021-12-07 11:52:49 -0500

    Throwing Knives
    Knives work again and can be thrown again. Further changes will be made soon.

--- Webb Game/Assets/Scripts/Weapons/Throwing Knife/throwKnife.cs
+++ Webb Game/Assets/Scripts/Weapons/Throwing Knife/throwKnife.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class throwKnife : MonoBehaviour
 {
     [SerializeField] public GameObject knifeParent;
+    [SerializeField] public GameObject knifeTrash;
     [SerializeField] public GameObject knife;
     [SerializeField] public GameObject player;
     [SerializeField] public float force;
@@ -13,25 +14,44 @@ public class throwKnife : MonoBehaviour
     [SerializeField] public float xoffset;
     [SerializeField] public float yoffset;
     [SerializeField] public float zoffset;
+    [SerializeField] public double cooldown;
 
 
     void Start()
     {
-        // var newInstance = Instantiate(knife, transform.position+(transform.forward) + new Vector3(0,0.45f,0), (transform.rotation));
-        // newInstance.parent = player;
+        // SpawnKnife();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse2)){
-            var newInstance = Instantiate(knife, transform.position + new Vector3(0,0,+2), (transform.rotation));
+        ThrowKnife();
+        // if(cooldown > 0)
+        // {
+        //     cooldown -= Time.deltaTime;
+        // } else if(cooldown ) {
+        //     cooldown = -0.01;
+        //     var newInstance = Instantiate(knife, transform.position, (transform.rotation));
+        //     newInstance.transform.SetParent(knifeParent.transform);
+        // }
+    }
+    public void ThrowKnife()
+    {
+        if(cooldown<1.5 && Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            var newInstance = Instantiate(knife, transform.position, (transform.rotation));
             newInstance.transform.SetParent(knifeParent.transform);
-            // var newInstance = Instantiate(knife, transform.position+(transform.forward) + new Vector3(0,0.45f,0), (transform.rotation));
-            // Rigidbody instance = newInstance.GetComponent<Rigidbody>();
-            // instance.maxAngularVelocity = maxSpin;
-            // instance.AddRelativeForce(Vector3.forward * force);
-            // instance.AddRelativeTorque(torque, 0, 0);
+            Rigidbody knifeRigidbody = newInstance.AddComponent<Rigidbody>();
+            knifeRigidbody.maxAngularVelocity = maxSpin;
+            knifeRigidbody.AddRelativeForce(Vector3.forward * force);
+            knifeRigidbody.AddRelativeTorque(torque, 0, 0);
+            newInstance.transform.SetParent(knifeTrash.transform);
+            // cooldown += 1.5;
         }
     }
+    // public void SpawnKnife()
+    // {
+    //     var newInstance = Instantiate(knife, transform.position, (transform.rotation));
+    //     newInstance.transform.SetParent(knifeParent.transform);
+    // }
 }
