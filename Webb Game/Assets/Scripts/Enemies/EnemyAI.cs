6f43a5d55b007b80243d460a2b7cfcc9158721e8
Dale Spencer <thefallout4fan@gmail.com>
2021-12-08 12:29:02 -0500

    Improved Enemy
    Gave Enemy a Weapon
    Gave Enemy's weapon an attack animation, working cooldown and all that.

--- Webb Game/Assets/Scripts/Enemies/EnemyAI.cs
+++ Webb Game/Assets/Scripts/Enemies/EnemyAI.cs
@@ -5,18 +5,35 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
+    public Animator animator;
+    [SerializeField] public double cooldownStatic;
+    public double cooldown = 0;
     [SerializeField] private Transform movePositionTransform;
+    [SerializeField] private double attackRange;
     private NavMeshAgent navMeshAgent;
+    [SerializeField] public GameObject weapon;
     private void Awake() {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = weapon.GetComponent<Animator>();
     }
 
     private void Update() {
-        Debug.Log(Vector3.Distance(movePositionTransform.position, transform.position));
-        if(Vector3.Distance(movePositionTransform.position, transform.position) > 3) {
+        if(Vector3.Distance(movePositionTransform.position, transform.position) > attackRange) {
             navMeshAgent.destination = movePositionTransform.position;
         } else {
             navMeshAgent.destination = transform.position;
+            transform.LookAt(movePositionTransform, Vector3.up);
+            }
+
+        if(cooldown <= 0 && Vector3.Distance(movePositionTransform.position, transform.position) < attackRange) {
+            animator.SetTrigger("Mace Attack 1");
+            cooldown = 0;
+            cooldown += cooldownStatic;
+        }
+        if(cooldown > 0) {
+            cooldown -= Time.deltaTime;
+        } else if(cooldown < 0) {
+            cooldown = 0;
         }
     }
 }
